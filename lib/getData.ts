1ae8c9f4eabb242f2cf1ddad414b42b62b7dc11b
Mohammed Abaherah <abaherah@bu.edu>
2025-11-04 17:21:01 -0500

    updated stuff

--- lib/getData.ts
+++ lib/getData.ts
@@ -1,7 +1,7 @@
 const API_KEY=process.env.API_KEY
 export default async function getData() {
     try {
-        const res = await fetch(`https://api.bigbookapi.com/search-books?api-key=${API_KEY}&query=books+about+wizards`,
+        const res = await fetch(`https://api.bigbookapi.com/search-books?api-key=${API_KEY}&query=books+about+children`,
             {
                 // ChatGPT told me to add this to ensure it never runs on the client
                 cache: "no-store",
